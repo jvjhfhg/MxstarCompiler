a1fe683dd47cf199395ecc533fd972c63bad53e6
Ubuntu-jvjhfhg <jvjhfhg@126.com>
2019-03-27 01:40:41 +0800

    arranged

--- Parser/run_test.sh
+++ Parser/run_test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-4.7.2-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
+javac *.java
 grun mxstar compilationUnit -tree < test/test.mx > test/out
+rm *.class
